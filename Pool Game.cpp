d8e74e6ded4e8f752485165dfc443feb0a751daa
Owen Prosser <owenjprosser@gmail.com>
2019-01-05 21:24:11 +0000

    Displaying scores and winner

--- Pool Game.cpp
+++ Pool Game.cpp
@@ -7,6 +7,7 @@
 #include<glut.h>
 #include<math.h>
 #include"simulation.h"
+#include <string> 
 
 //cue variables
 float gCueAngle = 0.0;
@@ -36,6 +37,111 @@ bool gCamZout = false;
 
 //rendering options
 #define DRAW_SOLID	(0)
+
+void DrawTextToScreen()
+{
+	char menu[80];
+	string output = "";
+	output = "Player " + std::to_string(gTable.currentPlayer+1);
+
+	strcpy(menu, output.c_str());
+	int len;
+	len = strlen(menu);
+
+	glColor3f(1, 1, 1);
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+
+	gluOrtho2D(0, 600, 0, 600);
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+
+	glLoadIdentity();
+
+	glRasterPos2i(5, 580);
+
+	for (int i = 0; i < len; ++i)
+	{
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, menu[i]);
+	}
+
+	glPopMatrix();
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);///////////////////////
+
+	output = "Player 1 Strokes: " + std::to_string(gTable.players[0].strokes) + " - "
+			"Player 2 Strokes: " + std::to_string(gTable.players[1].strokes);
+
+	strcpy(menu, output.c_str());
+	len = strlen(menu);
+
+	glColor3f(1, 1, 1);
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+
+	gluOrtho2D(0, 600, 0, 600);
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+
+	glLoadIdentity();
+
+	glRasterPos2i(400, 580);
+
+	for (int i = 0; i < len; ++i)
+	{
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, menu[i]);
+	}
+
+	glPopMatrix();
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+
+	if (gTable.currentHole >= 4) {///////////////////////////
+		output = "";
+		if (gTable.players[0].strokes < gTable.players[1].strokes) output = "Player One Wins!";
+		if (gTable.players[0].strokes > gTable.players[1].strokes) output = "Player Two Wins!";
+		if (gTable.players[0].strokes == gTable.players[1].strokes) output = "It's A Draw!";
+
+		strcpy(menu, output.c_str());
+		len = strlen(menu);
+
+		glColor3f(1, 1, 1);
+
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		glLoadIdentity();
+
+		gluOrtho2D(0, 600, 0, 600);
+
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+
+		glLoadIdentity();
+
+		glRasterPos2i(190, 300);
+
+		for (int i = 0; i < len; ++i)
+		{
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, menu[i]);
+		}
+
+		glPopMatrix();
+
+		glMatrixMode(GL_PROJECTION);
+		glPopMatrix();
+		glMatrixMode(GL_MODELVIEW);
+	}
+}
 
 void DoCamera(int ms)
 {
@@ -131,6 +237,8 @@ void DoCamera(int ms)
 
 void RenderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	DrawTextToScreen();
 
 	//set camera
 	glLoadIdentity();
@@ -429,41 +537,6 @@ void UpdateScene(int ms)
 
 	glutTimerFunc(SIM_UPDATE_MS, UpdateScene, SIM_UPDATE_MS);
 	glutPostRedisplay();
-}
-
-void DrawTextToScreen()
-{
-	char menu[80];
-	strcpy(menu, "Have courage and be kind");
-	int len;
-	len = strlen(menu);
-
-	glColor3f(1, 1, 1);
-
-	glMatrixMode(GL_PROJECTION);
-	glPushMatrix();
-	glLoadIdentity();
-
-	gluOrtho2D(0, 600, 0, 600);
-
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-
-	glLoadIdentity();
-
-	glRasterPos2i(190, 300);
-
-
-	for (int i = 0; i < len; ++i)
-	{
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, menu[i]);
-	}
-
-	glPopMatrix();
-
-	glMatrixMode(GL_PROJECTION);
-	glPopMatrix();
-	glMatrixMode(GL_MODELVIEW);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
